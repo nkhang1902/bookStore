8c972e88389aee1f6cfd4a6504696094c87cdcec
nhatminhdev <nhatminhc14@gmail.com>
2023-04-17 16:45:17 +0700

    details_UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import Layout from '../layout/Layout'
+import React from 'react';
+import Layout from '../layout/Layout';
+import BookDetails from './BookDetails/BookDetails';
 
 const Home = () => {
-  return (
-    <div>
-      <Layout>
-        <div class='center'><text>Hello World</text></div>
-      </Layout>
-    </div>
-  )
-}
+	return (
+		<div>
+			<Layout>
+				<div class='center'>
+					<text>Hello World</text>
+				</div>
+				<BookDetails />
+			</Layout>
+		</div>
+	);
+};
 
-export default Home
+export default Home;
