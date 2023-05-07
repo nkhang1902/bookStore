cf3ee2bafbe38b060bc6eb818ba2e5e32dfe2cb4
thinhluongit <62284881+thinhluongit@users.noreply.github.com>
2023-05-07 20:24:34 +0700

    update login.jsx
    upload login function

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import css from "./login.module.css";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/config";
+import { Link } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
-  const [pass, setPass] = useState("");
+  const [password, setPassword] = useState("");
+  // const history = useHistory();
 
-  const handleLogin = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+        console.log("Đăng nhập thành công");
+        // history.push("/");
+        // return <Link to="/signup"/>
+      })
+      .catch((error) => {
+        console.log(error);
+        console.log("Đăng nhập thất bại");
+      });
   };
 
   return (
@@ -13,23 +30,36 @@ const Login = () => {
       <div className={css.heading}>Login as Existing Customer</div>
       <form className={css.loginForm} onSubmit={handleLogin}>
         <label for="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          id="email"
+          name="email"
+        />
         <label for="password">Password</label>
-        <input type="password" id="password" name="password" />
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          secureTextEntry={true}
+        />
         <div className={css.rememberMe}>
           <input type="checkbox" /> Remember me
         </div>
 
         <p>
           By creating an account, you agree to Bookshop’s{" "}
-          <a href={""}>Privacy Notice</a> and <a href={""}>Terms of Use</a>.
+          <Link to="">Privacy Notice</Link> and <Link to="">Terms of Use</Link>.
         </p>
-        <button className={css.buttonLogin} type="summit">
+        <button className={css.buttonLogin} type="submit">
           LOGIN
         </button>
         <p>
-          or <a href={""}>Create a new account</a> |{" "}
-          <a href={""}>Forgot Password?</a>
+          or <Link to="/signup">Create a new account</Link> |
+          <Link to="">Forgot Password?</Link>
         </p>
       </form>
     </div>
