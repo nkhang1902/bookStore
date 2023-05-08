9d465a42199b29b168a8b396d9f8b35bfd954838
thinhluongit <62284881+thinhluongit@users.noreply.github.com>
2023-05-08 18:54:05 +0700

    update signup.jsx
    complete signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,35 +1,55 @@
 import React, { useState } from "react";
 import css from "./signup.module.css";
-import { auth } from "../firebase/config";
+import { db, auth } from "../firebase/config";
 import { Link } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import { collection, addDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const [dateOfBirth, setDateOfBirth] = useState(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  let navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       console.error("Passwords do not match!");
     }
-    console.log("sigup");
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log("userSignUp", user);
-        console.log("Đăng ký thành công");
-        return <Link to="/login"/>
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(error);
-        console.log("Đăng ký thất bại");
-      });
+    try {
+      console.log("sigup");
+      createUserWithEmailAndPassword(auth, email, password).then(
+        async (userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          await addDoc(collection(db, "User"), {
+            email: email,
+            fullName: firstName + lastName,
+            phone: phoneNumber,
+            dob: dateOfBirth,
+            address: address,
+            favourite: [],
+            cart: [],            
+          });
+          console.log("userSignUp", user);
+          console.log("Đăng ký thành công");
+          navigate('/login');
+        }
+      );
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(error);
+      console.log("Đăng ký thất bại");
+    }
   };
+
   return (
     <div className={css.container}>
       <div className={css.heading}>Create an Account</div>
@@ -38,36 +58,95 @@ const SignUp = () => {
         and order history.
       </div>
       <form className={css.signUpForm}>
-        <label htmlFor="email">Email</label>
+        <div className={css.nameBox}>
+          <div>
+            <label htmlFor="firstName">
+              First Name<span>*</span>
+            </label>
+            <input
+              className={css.firstNameBox}
+              type="name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">
+              Last Name<span>*</span>
+            </label>
+            <input
+              className={css.lastNameBox}
+              type="name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          </div>
+        </div>
+
+        <label htmlFor="phoneNumber">
+          Phone Number<span>*</span>
+        </label>
+        <input
+          type="tel"
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
+          required
+        />
+        <label htmlFor="dateOfBirth">Date of birth<span>*</span></label>
+        <input
+          type="date"
+          value={dateOfBirth}
+          onChange={(e) => setDateOfBirth(e.target.value)}
+          required
+        />
+        <label htmlFor="address">Address</label>
+        <input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}        
+        />
+
+        <label htmlFor="email">
+          Email<span>*</span>
+        </label>
         <input
           type="email"
           id="email"
           name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          required
         />
-        <label htmlFor="password">Password</label>
+        <label htmlFor="password">
+          Password<span>*</span>
+        </label>
         <input
           type="password"
           id="password"
           name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          required
         />
-        <label htmlFor="passwordConfirm">Password Confirmation</label>
+        <label htmlFor="passwordConfirm">
+          Password Confirmation<span>*</span>
+        </label>
         <input
           type="password"
           id="passwordConfirm"
           name="passwordConfirm"
           value={confirmPassword}
           onChange={(e) => setConfirmPassword(e.target.value)}
+          required
         />
         <div className={css.checkBox}>
           <input type="checkbox" /> Send me emails about new books, authors, and
           bookstores from Bookshop.
         </div>
 
-        <p style={{marginTop: '10px'}}>
+        <p>
           By creating an account, you agree to Bookshop’s{" "}
           <a href={""}>Privacy Notice</a> and <a href={""}>Terms of Use</a>.
         </p>
