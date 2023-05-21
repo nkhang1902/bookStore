839b6edbb2993b6629e94264dd0254fd0c6527c2
nhatminhdev <heomeco2014@gmail.com>
2023-05-22 00:17:41 +0700

    fix: add css for layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,16 +1,24 @@
 import Footer from '../components/Footer'
 import Navbar from '../components/Nav'
 
-const Layout = ({children}) => {
- return (
-  <div>
-    <>
-    <Navbar/>
-    <main style={{ marginTop: '70px', paddingTop: '60px' }}>{children}</main>
-    <Footer />
-    </>
-  </div>
- )
+const Layout = ({ children }) => {
+    return (
+        <div>
+            <>
+                <Navbar />
+                <main
+                    style={{
+                        marginTop: '70px',
+                        paddingTop: '60px',
+                        minHeight: '86vh',
+                    }}
+                >
+                    {children}
+                </main>
+                <Footer />
+            </>
+        </div>
+    )
 }
 
 export default Layout
